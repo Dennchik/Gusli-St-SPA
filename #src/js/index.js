3c05b4dd3a1d58dd7c78f09657105168c09309df
Alexandr <104072970+Dennchik@users.noreply.github.com>
2024-10-30 14:18:01 +0300

    30 Okt 2024

--- #src/js/index.js
+++ #src/js/index.js
@@ -1,6 +1,6 @@
 import React, {StrictMode} from 'react';
 import {createRoot} from 'react-dom/client';
-import {HashRouter as Router,} from 'react-router-dom';
+import {BrowserRouter as Router,} from 'react-router-dom';
 // import {HashRouter} from 'react-router-dom';
 import App from '../js/app/App.js';
 
